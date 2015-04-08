67a500cf61f854bfaae42c4baace89dc76bbd575
Art-bell <belloayo24@gmail.com>
2015-04-08 11:26:31 -0700

    yolo2

--- silver_weather/admin.py
+++ silver_weather/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from silver_weather.models import BetterUser, RainFavourite, SunFavourite, SnowFavourite
 
-# Register your models here.
+admin.site.register(BetterUser)
+admin.site.register(RainFavourite)
+admin.site.register(SunFavourite)
+admin.site.register(SnowFavourite)
